2ca78f4cb75359ae4e154c563998328344ee3663
unnati-14 <singhunnati@gmail.com>
2022-07-31 17:03:16 +0530

    fq

---
+++ sticker.py
@@ -0,0 +1,11 @@
+def solve(a, n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return a[0]
+    dp = [0]*n
+    dp[0] = a[0]
+    dp[1] = max(a[0],a[1])
+    for i in range(2,n):
+        dp[i] = max(a[i]+dp[i-2],dp[i-1])
+    return dp[-1]
